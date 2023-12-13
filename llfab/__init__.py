e150e88b8f24025a14aaf549cd1a49a53e889b10
Andrew Bogdan <andrewbogdan@berkeley.edu>
2023-12-13 12:46:33 -0800

    Added a logfile that will, by default, log everything above the logging.DEBUG level. Modified the config file finding to work on both machines.

--- llfab/__init__.py
+++ llfab/__init__.py
@@ -3,22 +3,42 @@ I'm gonna make small lenses with a 6-axis and a laser.
 - Andrew
 """
 
+import logging
 import os
 import yaml
 
+_logger = logging.getLogger(__name__)
 
-# Global constants
-dir_path = os.sep.split(__file__)[:-2]
-CONFIG_FILE = r'C:\Users\LaserLab\Andrew\LensletFab\config.yaml'
-# os.path.join(*dir_path, 'config.yaml')
-#
-
+# --- Config ------------------------------------------------------------------
+CONFIG_ROOT = os.path.abspath(os.path.join(
+    os.path.abspath(__file__), '..', '..'
+))
+CONFIG_FILE = os.path.join(CONFIG_ROOT, 'config.yaml')
 
 # Load the config file
-with open(CONFIG_FILE, 'r') as config_file:
-    config = yaml.safe_load(config_file.read())
+config = None
+try:
+    with open(CONFIG_FILE, 'r') as config_file:
+        config = yaml.safe_load(config_file.read())
+    del config_file
+except FileNotFoundError as err:
+    _logger.error(f'Config file \'{CONFIG_FILE}\' not found')
+    raise err
 
+# --- Logging -----------------------------------------------------------------
+LOGFILE = os.path.join(CONFIG_ROOT, f'{__name__}.log')
+LOGFILE_FMT = '%(asctime)s: %(message)s'
 
-# Delete unnecessary variables
-del yaml
-del config_file
+_logger.setLevel(logging.DEBUG)
+_handler_logfile = logging.FileHandler(LOGFILE)
+_handler_logfile.setFormatter(logging.Formatter(LOGFILE_FMT))
+_handler_logfile.setLevel(logging.DEBUG)
+_logger.addHandler(_handler_logfile)
+_logger.debug(
+    f'Package {__name__} loaded with logfile {LOGFILE}.\n'
+    f'To disable logging, use logging.getLogger({__name__}).setLevel or '
+    f'set logging.getLogger({__name__}).propagate = False.'
+)
+
+# --- Package Scope -----------------------------------------------------------
+__all__ = []
